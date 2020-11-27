90cb93ce3f08cd5e46d7d9dc4a137396955e851a
Wesle Leal <wesle.profissional@gmail.com>
2020-11-27 09:03:31 -0300

    generating token JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,49 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    // user.password - Senha criptografada
+    // password - Senha não-criptografada
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    // Usuário autenticado
+
+    const token = sign({}, '698dc19d489c4e4db73e28a713eab07b', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
