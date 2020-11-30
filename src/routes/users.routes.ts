76f91128b0d7aad0246c56f41206af74131a5dc0
Wesle Leal <wesle.profissional@gmail.com>
2020-11-30 06:16:51 -0300

    atualizando avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -47,8 +47,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user: Request = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
